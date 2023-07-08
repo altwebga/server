f192d54dda162cbd88999ffdcbfc47d7d58075a4
Konstantin <altwebga@gmail.com>
2023-07-08 11:33:52 +0700

    fix RecaptchaV3

--- app.js
+++ app.js
@@ -30,6 +30,7 @@ app.use(
   })
 );
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.post("/message", async (req, res) => {
   recaptcha.verify(req, async (error, data) => {
